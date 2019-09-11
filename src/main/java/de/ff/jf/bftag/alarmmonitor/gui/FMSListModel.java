bbc131352a4326874f995334bd8775c52a67e199
Sebastian Schwegler <privat@sebastianschwegler.de>
2019-09-11 15:01:25 +0200

    implemented TTS output for FMS state change to 4 or 5

--- src/main/java/de/ff/jf/bftag/alarmmonitor/gui/FMSListModel.java
+++ src/main/java/de/ff/jf/bftag/alarmmonitor/gui/FMSListModel.java
@@ -1,6 +1,7 @@
 package de.ff.jf.bftag.alarmmonitor.gui;
 
 import de.ff.jf.bftag.alarmmonitor.models.Car;
+import de.ff.jf.bftag.alarmmonitor.workflow.TextToSpeech;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -9,9 +10,10 @@ import java.util.stream.Collectors;
 
 public class FMSListModel extends DefaultListModel<Car> {
     private List<Car> elements = new ArrayList<>();
+    private TextToSpeech textToSpeech;
 
     public FMSListModel() {
-
+        textToSpeech = new TextToSpeech();
     }
 
     @Override
@@ -26,6 +28,12 @@ public class FMSListModel extends DefaultListModel<Car> {
 
     public int updateFMSState(String car, int state) {
         elements.stream().filter(e -> e.getName().equalsIgnoreCase(car)).collect(Collectors.toList()).get(0).setFms(state);
+        if (state == 4) {
+            textToSpeech.speak(car + " ist an der Einsatzstelle angekommen.");
+        }
+        if (state == 5) {
+            textToSpeech.speak(car + " hat einen Sprechwunsch!");
+        }
         fireContentsChanged(this, 0, 1);
         return state;
     }
